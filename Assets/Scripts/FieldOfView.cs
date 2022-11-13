f026bfe5c27bce3ae1260bee1592bed96cec9333
Arpit Mathur <arpitmathur19@gmail.com>
2022-11-13 17:48:59 -0500

    Recovered lost commit and added assets

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -10,10 +10,14 @@ public class FieldOfView : MonoBehaviour
     [SerializeField] private float viewDistance;
     [SerializeField] private float viewAngle;
     [SerializeField] private int rayCount;
+    [SerializeField] private float zOffset;
+
+    private LayerMask layerMask;
 
     private Vector3 origin;
     private float startingAngle;
-    private float deltaAngle;
+
+    private Camera cam;
 
     // Start is called before the first frame update
     void Start()
@@ -21,16 +25,31 @@ public class FieldOfView : MonoBehaviour
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
-        origin = Vector3.zero;
+        origin = new Vector3(0,0,zOffset);
         startingAngle = viewAngle / 2;
 
-        deltaAngle = viewAngle / rayCount;
+        layerMask = LayerMask.GetMask("Obstacle");
+
+        cam = Camera.main;
+    }
+
+    void Update() {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 0));
+        Vector3 playerToMouse = point - origin;
+        float turnToMouseAngle = Mathf.Atan((playerToMouse.y) / (playerToMouse.x));
+        if (playerToMouse.x < 0) {
+            SetStartingAngle(turnToMouseAngle + (180) * Mathf.Deg2Rad);  
+        } else {
+            SetStartingAngle(turnToMouseAngle);        
+        }
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         float angle = startingAngle;
+        float deltaAngle = viewAngle / rayCount;
 
         int[] triangles = new int[(rayCount - 1) * 3];
         Vector3[] vertices = new Vector3[rayCount + 1];
@@ -40,18 +59,32 @@ public class FieldOfView : MonoBehaviour
 
             Vector2 direction = new Vector2(viewDistance * Mathf.Cos(angle), viewDistance * Mathf.Sin(angle));
 
-            vertices[i] = new Vector3(direction.x, direction.y, 0);
+            RaycastHit2D hit = Physics2D.Raycast(origin, direction, viewDistance, layerMask);
+
+            if (hit.collider == null) {
+                vertices[i] = new Vector3(direction.x + origin.x, direction.y + origin.y, origin.z);
+            } else {
+                vertices[i] = new Vector3(hit.point.x, hit.point.y, origin.z);
+            }
 
             angle -= deltaAngle;
 
-            if (i < rayCount - 1) {
-                triangles[(i - 1) * 3 + 0] = 0;
-                triangles[(i - 1) * 3 + 1] = i + 1;
-                triangles[(i - 1) * 3 + 2] = i;
+            if (i > 1) {
+                triangles[(i - 2) * 3 + 0] = 0;
+                triangles[(i - 2) * 3 + 1] = i - 1;
+                triangles[(i - 2) * 3 + 2] = i;
             }
         }        
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+    }
+
+    public void SetOrigin(Vector2 origin) {
+        this.origin = new Vector3(origin.x, origin.y, zOffset);
+    }
+
+    private void SetStartingAngle(float anglePlayer) {
+        this.startingAngle = anglePlayer + viewAngle / 2;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,11 +10,17 @@ public class PlayerMovement : MonoBehaviour
 
     private float move_x, move_y;
 
+    private GameObject fieldOfView_go;
+    private FieldOfView fieldOfView_script;
+
     private void Start() {
         player_rb = GetComponent<Rigidbody2D>();
 
         move_x = 0f;
         move_y = 0f;
+
+        fieldOfView_go = GameObject.FindGameObjectsWithTag("FieldOfView")[0];
+        fieldOfView_script = fieldOfView_go.GetComponent<FieldOfView>();
     }
 
     // Update is called once per frame
@@ -26,5 +32,7 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate() {
         player_rb.velocity = new Vector2(move_x, move_y);
+
+        fieldOfView_script.SetOrigin(new Vector2(transform.position.x, transform.position.y));
     }
 }
