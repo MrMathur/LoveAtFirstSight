383eea707db0d4df8ddc49bc4ddfce32d1573f0a
Arpit Mathur <arpitmathur19@gmail.com>
2022-11-18 02:17:46 -0500

    Enemy movement mechanic

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] private GameObject waypoints;
+    [SerializeField] private bool followWaypoints;
+    [SerializeField] private float speed;
+    [SerializeField] private float timeDelay;
+    [SerializeField] private float distanceThreshold;
+
+    private Transform currentTarget;
+    private Transform[] targets;
+    private int targetIndex;
+    private bool delayCondition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targets = new Transform[waypoints.GetComponentsInChildren<Transform>().GetLength(0)];
+
+        int i = 0;
+        foreach (Transform t in waypoints.GetComponentsInChildren<Transform>())
+        {
+            targets[i] = t;
+            i++;
+        }
+
+        targetIndex = 0;
+        currentTarget = targets[targetIndex];
+
+        delayCondition = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followWaypoints) {
+
+            float distance = Vector2.Distance(transform.position, currentTarget.position);
+
+            if (distance < distanceThreshold) {
+                if (delayCondition) {
+                    StartCoroutine(SetDelay());
+                }
+            } else {
+                float step = speed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, step);
+            }
+            
+        }
+    }
+
+    IEnumerator SetDelay() {
+        delayCondition = false;
+        yield return new WaitForSeconds(timeDelay);
+        targetIndex = (targetIndex + 1) % targets.Length;
+        currentTarget = targets[targetIndex];
+        delayCondition = true;
+    }
+}
