d7db2f1840a3ad6119326f0b18cdae178bdaf1fd
Arpit Mathur <arpitmathur19@gmail.com>
2022-12-01 03:22:14 -0500

    ui_changes

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -10,21 +10,13 @@ public class MenuFunctions : MonoBehaviour
     private int levelSelectIndex = 1;
     private int startIndex = 2;
     private int creditsIndex;
-
-    [SerializeField] private bool startPaused;
     
     private GameObject env;
-    private GameObject modal;
 
     private void Start() {
         env = GameObject.FindGameObjectsWithTag("Environment")[0];
-        creditsIndex = SceneManager.sceneCountInBuildSettings-1;
-
-        modal = GameObject.FindGameObjectsWithTag("Modal")[0];
-
-        if (startPaused) {
-            Time.timeScale = 0;
-        }
+        
+        creditsIndex = SceneManager.sceneCountInBuildSettings-1;     
     }
 
     public void StartGame() {       
@@ -50,15 +42,5 @@ public class MenuFunctions : MonoBehaviour
 
     public void ToggleMute() {
         env.GetComponent<AudioSource>().mute = !env.GetComponent<AudioSource>().mute;
-    }
-
-    public void closeModal() {
-        this.modal.SetActive(false);
-        Time.timeScale = 1;
-    }
-
-    public void openModal() {
-        modal.SetActive(true);
-        Time.timeScale = 0;
     }
 }
